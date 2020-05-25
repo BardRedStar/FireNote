871a4e8184277e9daca77415b21d7276272cdd03
BardRedStar <den.kovalev999@gmail.com>
2020-05-25 12:55:02 +0700

    Initial commit

---
+++ Application/FireNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireNote
+//
+//  Created by Denis Kovalev on 25.05.2020.
+//  Copyright © 2020 Denis Kovalev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
