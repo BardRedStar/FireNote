7099b62673655f046ed535c23394af7a8560ba82
BardRedStar <den.kovalev999@gmail.com>
2020-07-15 17:13:01 +0700

    Add the resources folder for assets and fonts.

---
+++ Application/FireNote/Modules/Editor/EditorAttachments/EditorAttachmentCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  EditorAttachmentCollectionViewCell.swift
+//  FireNote
+//
+//  Created by Denis Kovalev on 15.07.2020.
+//  Copyright © 2020 Denis Kovalev. All rights reserved.
+//
+
+import Foundation
